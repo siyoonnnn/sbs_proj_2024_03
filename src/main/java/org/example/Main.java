c7db7374f992dd23d8856994c4c3861a90e7ac6e
siyoonnnn <tldbs2@naver.com>
2024-03-20 14:30:25 +0900

    게시물 작성기능 구현 중

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,46 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("== 프로젝트 시작 ==");
+        Scanner sc = new Scanner(System.in);
+
+        int lastArticleId = 0;
+
+        while ( true ) {
+            System.out.printf("명령어) ");
+            String cmd = sc.nextLine();
+            cmd = cmd.trim();
+
+
+
+            if (cmd.length() == 0) {
+                continue;
+            }
+
+            if (cmd.equals("exit")) {
+                break;
+            }
+            if (cmd.equals("article write")) {
+                int id = lastArticleId + 1;
+                lastArticleId = id;
+                System.out.println("제목: ");
+                String title = sc.nextLine();
+                System.out.println("내용: ");
+                String body = sc.nextLine();
+
+                System.out.printf("#d번 글이 생성되었습니다.");
+            }
+            else if (cmd.equals("article list")) {
+                System.out.println("게시물이 없습니다.");
+            } else {
+                System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", cmd);
+            }
+        }
+
+        sc.close();
+        System.out.println("== 프로젝트 끝 ==");
     }
 }
